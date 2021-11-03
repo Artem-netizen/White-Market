b90f7a9288ead8635202252200f9a83f9a2501a1
Artyom Besedin <crazytorch1@gmail.com>
2021-11-04 04:04:24 +0800

    White-Market

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+var t1 = new TimelineMax({ paused: true });
+
+t1.to(".one", 0.2, {
+    y: 6,
+    rotation: 45,
+    ease: Expo.easeInOut
+});
+t1.to(".two", 0.2, {
+    y: -6,
+    rotation: -45,
+    ease: Expo.easeInOut,
+    delay: -0.4
+});
+
+t1.to(".menu", 1, {
+    top: "0%",
+    ease: Expo.easeInOut,
+    delay: -1
+});
+
+t1.staggerFrom(".menu ul li", .1, { x: -200, opacity: 0, ease: Expo.easeOut }, 0.1);
+
+t1.reverse();
+$(document).on("click", ".toggle-btn", function() {
+    t1.reversed(!t1.reversed());
+});
+$(document).on("click", ".data__link", function() {
+    t1.reversed(!t1.reversed());
+});
